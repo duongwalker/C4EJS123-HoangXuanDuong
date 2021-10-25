b43f63db4d6adb951fb55a57a173078f45a3a37b
duongwalker <duongtrick1@gmail.com>
2021-10-25 09:12:06 +0700

    Bai tap buoi 8

---
+++ Session 8/index.js
@@ -0,0 +1,71 @@
+// Bai 1
+// 1d 2c 3a 4b
+
+// Bai 2
+// {
+// setInterval(()=>{
+//     console.log ('Hello anh Thành')
+// }, 3000
+
+// )
+// };
+
+
+// Bai 3
+// Both variables declared by let and var can't still exist because variables declared within a JavaScript function, become LOCAL to the function.
+// => It can only be accessed from within the function.
+
+
+// Bai 4
+// {
+// var x = document.getElementsByTagName("li");
+// console.log (x[1]);
+
+// for (let i = 0; i< x.length; i++){
+//     console.log (x[i])
+// }
+// };
+
+// Bai 5
+
+// {
+//     var y = document.getElementsByTagName("div");
+//     console.log (y[1]);
+    
+//     for (let i = 0; i< y.length; i++){
+//         console.log (y[i])
+//     }
+// };
+
+
+// Bai 6
+
+// {
+// var el = document.getElementById('div-02');
+// el.remove();
+// };
+
+// Bai 7
+// document.getElementById("button1").addEventListener("click", (e) => {
+//     console.log (e.target)
+// }
+// )
+// document.getElementById("button2").addEventListener("click", (e) => {
+//     console.log (e.target)
+// }
+// )
+
+// document.addEventListener("keydown", (e) => {
+//     console.log (e.key)
+// }
+// )
+
+// Bai 8 - 10
+// function myFunction(name, wish) {
+//     alert (`My name is ${name}`)
+//     alert (`My wish in the next year is ${wish}`)
+// }
+// myFunction ('Hoang Xuan Duong', 'Studying abroad')
+
+// Bai 11
+
